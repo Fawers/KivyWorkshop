8f358baadb16faeb12a491e4f84c4cac69f307b1
Fawers <fabricioswerneck@gmail.com>
2015-04-12 13:56:26 -0300

    refactor usando kivy.uix.carousel.Carousel

--- Aula4/App/main.py
+++ Aula4/App/main.py
@@ -6,62 +6,29 @@ from __future__ import unicode_literals
 from random import shuffle
 
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.carousel import Carousel
 
 
-__version__ = '0.1'
+__version__ = '0.2'
 
-class ScreenTraverserMixin(object):
-    def goto_screen(self, number):
-        current_screen_name = self.manager.current
-        current_screen_number = self.manager.screen_names.index(current_screen_name)
-        target_screen_name = self.manager.screen_names[number]
-
-        if current_screen_number < number:
-            self.manager.transition.direction = 'left'
-        elif number < current_screen_number:
-            self.manager.transition.direction = 'right'
-
-        self.manager.current = target_screen_name
-
-
-class SplashScreen(ScreenTraverserMixin, Screen):
+class Root(Carousel):
     def __init__(self):
-        Screen.__init__(self, **{'name': 'splash'})
-
-
-class MainScreen(ScreenTraverserMixin, Screen):
-    def __init__(self):
-        Screen.__init__(self, **{'name': 'main'})
+        Carousel.__init__(self)
         from text.dilmes import DILMES
         self.dilmes = DILMES
 
-    def get_blocks(self, blocks):
+    def show_text(self, num_of_blocks):
         shuffle(self.dilmes)
-        return '\n\n'.join(self.dilmes[:blocks])
-
-    def show_text(self, number):
-        txt = self.manager.screens[-1].ids['txt']
-        txt.text = self.get_blocks(number)
-        self.goto_screen(2)
-
-
-class TextScreen(ScreenTraverserMixin, Screen):
-    def __init__(self):
-        Screen.__init__(self, **{'name': 'text'})
+        text = '\n\n'.join(self.dilmes[:num_of_blocks])
+        self.ids['target_txt'].text = text
+        self.load_next()
 
 
 class DilmesApp(App):
     def build(self):
-        self.sm = ScreenManager()
-        self.sm.add_widget(SplashScreen())
-        self.sm.add_widget(MainScreen())
-        self.sm.add_widget(TextScreen())
+        self.root = Root()
 
-        return self.sm
+        return self.root
 
 
 DilmesApp().run()
